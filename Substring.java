63c6e367c944fef71c2ed94589714d1934eb697f
Aditya <aditya.a2822007@gmail.com>
2026-09-23 01:12:56 +0530

    Add Substring example in Substring.java

---
+++ Substring.java
@@ -0,0 +1,10 @@
+class Main
+{
+  public static void main(String[] args)
+  {
+      String s = "Java is easy";
+      
+      System.out.println(s.substring(6));
+      System.out.println(s.substring(0, 6));
+  }
+}
