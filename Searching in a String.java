fd2bec574caa0774bac2985aa7109ff9db684dc3
Aditya <aditya.a2822007@gmail.com>
2026-09-23 00:42:04 +0530

    Add string searching examples in Searching in a String.java

---
+++ Searching in a String.java
@@ -0,0 +1,14 @@
+class Main
+{
+  public static void main(String[] args)
+  {
+    String s = "Hello";
+    String a = "Hello";
+    String b = "hello";
+
+      System.out.println(s.indexOf("l"));
+      System.out.println(s.indexOf("l",2));
+      System.out.println(s.lastIndexOf("l"));
+      System.out.println(s.contains("H"));
+  }
+}
